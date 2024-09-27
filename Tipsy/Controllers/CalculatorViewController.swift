5505026526f248a248c41c69b10fcc2345a54fda
My <uk472363@yanex.ru>
2024-09-27 08:38:00 +0300

    commit

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,17 +16,59 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
     
+    var currentTip = ""
+    var summ = 0.0
     
-
     @IBAction func tipChanged(_ sender: UIButton) {
+       
+        
+        switch sender {
+        case twentyPctButton:
+            twentyPctButton.isSelected = true
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            currentTip = twentyPctButton.title(for: .selected) ?? ""
+            
+        case tenPctButton:
+            tenPctButton.isSelected = true
+            zeroPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+            currentTip = tenPctButton.title(for: .selected) ?? ""
+        default:
+            zeroPctButton.isSelected = true
+            twentyPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            currentTip = zeroPctButton.title(for: .selected) ?? ""
+        }
+        
+        print(currentTip)
+        
+       
     }
     
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
+        
+        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let afterPercent = ((Double(billTextField.text ?? "") ?? 0.0) * (Double(currentTip.dropLast()) ?? 0)) / 100
+        summ = (afterPercent + (Double(billTextField.text ?? "") ?? 0.0)) / (Double(splitNumberLabel.text ?? "") ?? 0.0)
+        print(summ)
+        
+        self.performSegue(withIdentifier: "tipSegue", sender: self)
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tipSegue" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.totalLabelText = String(summ)
+            destinationVC.settingsLabelText = "Tip: \(currentTip), Split: \(splitNumberLabel.text ?? "")"
+        }
+    }
 }
 
